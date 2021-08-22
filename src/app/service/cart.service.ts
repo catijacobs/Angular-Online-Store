5997350f90f17fe6284b55c45ee2c98532f077aa
Catia Jacob <catia.jacob@inovflow.local>
2021-08-22 20:55:04 +0100

    fix add to cart and consolelog

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -22,8 +22,7 @@ export class CartService {
     this.cartItemList.push(product);
     this.productList.next(this.cartItemList);
     this.getTotalPrice();
-
-    console.log(this.cartItemList);
+    console.log(this.cartItemList)
   }
   getTotalPrice(){
     let grandTotal = 0;
