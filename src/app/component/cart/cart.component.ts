1fe2b9c4f6c43611a9d5ea0331482b072c5b4a81
Catia Jacob <catia.jacob@inovflow.local>
2021-08-22 21:01:45 +0100

    fix getTotalPrice method in cartService

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  public product : any = [];
+  public grandTotal !: number;
+
+  constructor(private cartService : CartService) { }
 
   ngOnInit(): void {
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.grandTotal = this.cartService.getTotalPrice();
+    })
   }
 
 }
