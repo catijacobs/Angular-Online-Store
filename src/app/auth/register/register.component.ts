297c31a16ba1725fcd957a4a45636d75760ad387
Catia Jacob <catia.jacob@inovflow.local>
2021-08-23 03:07:41 +0100

    set TS files configuration

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  successMessage:string =""
+
+  regForm!:FormGroup
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.regForm = this.fb.group({
+      name: ['',[Validators.required]],
+      mobileNumber: ['',[Validators.required, Validators.min(1000000000),Validators.max(9999999999)]],
+      email:['',[Validators.required, Validators.pattern("[a-zA-Z0-9]*@gmail.com")]],
+      password: ['',[Validators.required,Validators.pattern("[a-zA-z@_]{6,}")]]
+    })
+  }
+
+  register(){
+    this.successMessage = "Successfully Registered..."
+    // console.log(this.regForm)
   }
 
 }
--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent{
 
   constructor() { }
 
-  ngOnInit(): void {
+  flag:boolean = true
+
+
+  apply(value:string){
+    this.flag = value == "login"?true : false;
   }
 
 }
